c9a061699b0b3fc4f6afb793d37bdd7fa7920b3d
Muhammad Ivan Riansyah Putra <33018078+shakellzero@users.noreply.github.com>
2020-05-16 20:42:05 +0800

    update main menu
    main menu

---
+++ SistemGameFP/Assets/HowToPlay.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class HowToPlay : MonoBehaviour
+{
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ SistemGameFP/Assets/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("level1");
+    }
+   
+    public void HowToPlay()
+    {
+        SceneManager.LoadScene("HowToPlayScene");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+}
